541cb906053a7fb38e311d97640ac674335c9820
tunglqgithub <tung.le_dp05@devplus.edu.vn>
2023-03-24 11:44:05 +0700

    feat: dpm-fix-responsive

---
+++ src/pages/FAQ/FAQ.js
@@ -0,0 +1,29 @@
+const OrderingAndPaymentQuestions = [
+    "How do I change or cancel my order?",
+    "I placed an order, but never received a confirmation email.",
+    "What payment methods do you accept?",
+    "I am an international customer and I am not sure how much I'll be charged for a purchase",
+];
+
+const TrackingOrderQuestions = [
+    "How do I check on the status of my order?",
+    "I am an international customer and I want to return an item.",
+    "My tracking info shows my package was delivered, but I never received it.",
+    "How long does it take to process returns?"
+];
+
+const SizeGuideQuestions = [
+    "I'm an international Fashionista. How do I find out what my size is?",
+    "What are the inseams of your jeans?",
+    "What is your turn around time? When can I expect to receive my hand made garment?",
+    "What if my clothing does not fit?"
+];
+
+const answer = `We’re super quick at processing orders to make sure that you receive them as soon as possible. 
+You can change your order as much as you like before you click “Check- out” but after this point you will 
+be unable to make changes to your order. Keep in mind our system is designed to fulfill orders as quickly 
+as possible. We cannot cancel or change an order once it has been placed. In such case you can however, 
+return the item to us in accordance with our Returns Policy. If we sent out your package and your items 
+are returned to us (Packages marked "Return to Sender") due to an incorrect/undeliverable address, 
+we will issue a store credit for the net total minus the shipping fee. You will be held responsible for 
+the initial cost of shipping the package.`;
